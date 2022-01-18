a0247616980536ce06c3ec4421354d66e016bcb1
Grigory Perevalov <biryusala@gmail.com>
2022-01-19 02:49:47 +0300

    FRONT PROD

--- front/src/components/admin/Admin.tsx
+++ front/src/components/admin/Admin.tsx
@@ -11,6 +11,14 @@ const Admin: FC<AdminProps> = () => {
   const [userToPromote, setUserToPromote] = useState('');
   const [songsContent, setSongsContent] = useState<any>(null);
   const [songName, setSongName] = useState('');
+  const [inputs, setInputs]: any = useState({
+    songGenre: '',
+    songArtist: '',
+    songName: ''
+  });
+  const [suggested, setSuggested] = useState<string[]>([]);
+  // @ts-ignore
+  const handleChange = (e: any) => setInputs((prevState: any) => ({ ...prevState, [e.target.name]: e.target.value }));
 
   async function searchSongs() {
     const songs = await fetch(`http://localhost:8080/song/getFiltred?name=${songName}&artist=&username=&genres=&offset=${0}&limit=${5}`)
@@ -25,8 +33,6 @@ const Admin: FC<AdminProps> = () => {
       acc.push(<div className={'song'}>
         <span className={'song-field song-title'}>{curr.name}</span>
         <span className={'song-field song-genres'}>Жанры: {curr.genres}</span>
-        <span className={'song-field song-year'}>Год выпуска: {curr.year}</span>
-        <span className={'song-field song-tags'}>Теги: {curr.tags}</span>
         <button className={'admin-userRemove'} onClick={() => deleteSongById(curr.songId)}>X</button>
       </div>);
       return acc;
@@ -34,9 +40,8 @@ const Admin: FC<AdminProps> = () => {
   }
 
   async function deleteUser(username: string) {
-    return await fetch(`http://localhost:8080/admin/deleteUser?username=${username}`);
+    const result = await fetch(`http://localhost:8080/admin/deleteUser?username=${username}`);
     await getUsers();
-
   }
 
   async function deleteSongById(songToDelete: number) {
@@ -46,6 +51,17 @@ const Admin: FC<AdminProps> = () => {
 
   async function promoteUserById() {
     return await fetch(`http://localhost:8080/admin/giveAdminRoot?username=${userToPromote}`);
+  }
+
+  async function addSong() {
+    return await fetch(`http://localhost:8080/song/add?name=${inputs.songName}&artist=${inputs.songArtist}&genres=${inputs.songGenre}`);
+  }
+
+  async function searchGenre(e: any) {
+    handleChange(e);
+    const text = e.target.value;
+    setSuggested((await fetch(`http://localhost:8080/genre/help?text=${text}&limit=${10}`)
+      .then((response: any) => response.json())).genres);
   }
 
   async function getUsers() {
@@ -71,7 +87,52 @@ const Admin: FC<AdminProps> = () => {
       <h2 className={'admin-header'}>Админская панель</h2>
       <div className={'admin-movies'}>
         <h2>Управление песнями</h2>
+        <h3>Добавить песню</h3>
+        <div className={'admin-searchFilter main-searchFilter'}>
+          <span className={'main-name'}>Название песни</span>
+          <input className={'main-search'}
+                 name={'songName'}
+                 value={inputs.songName}
+                 onChange={handleChange}/>
+        </div>
+        <div className={'admin-searchFilter main-searchFilter'}>
+          <span className={'main-name'}>Имя автора</span>
+          <input className={'main-search'}
+                 name={'songArtist'}
+                 value={inputs.songArtist}
+                 onChange={handleChange}/>
+        </div>
+        <div className={'admin-searchFilter main-searchFilter'}>
+          <span className={'main-name'}>Жанр</span>
+          <input className={'main-search'}
+                 name={'songGenre'}
+                 onBlur={() => {
+                   setTimeout(() => {
+                     setSuggested([]);
+                   }, 100);
+                 }}
+                 value={inputs.songGenre}
+                 onChange={searchGenre}/>
+          {suggested.length ?
+            <div className={'main-suggestedWords'}>
+              {suggested.map((word: string, index, array) => {
+                return (
+                  <div onClick={() => {
+                    setSuggested([]);
+                    setInputs((prevState: any) => ({ ...prevState, songGenre: word }));
+                  }} key={generateUniqueId()} className={'main-suggestedItem'}>
+                    <span className={'main-suggestedName'}>{word}</span>
+                    {index !== array.length - 1 ?
+                      <hr className={'main-suggestedLine'}/> : null}
+                  </div>
+                )}
+              )}
+            </div> : null
+          }
+          <button onClick={addSong} className={'admin-addMovieButton admin-addSongButton'}>Добавить песню</button>
+        </div>
         <div className={'admin-addMovie'}>
+          <h3>Удалить песню</h3>
           <span className={'admin-fieldName'}>Название</span>
           <input className={'admin-movieInput'} value={songName} onChange={(e: any) => setSongName(e.target.value)}/>
           <button onClick={searchSongs} className={'admin-addMovieButton'}>Искать</button>
